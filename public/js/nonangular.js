f84ffb61e06823cb3077afe4252b2ecb981722b0
pvtech <tech@golivelabs.io>
2015-02-11 12:28:20 -0800

    info

--- public/js/nonangular.js
+++ public/js/nonangular.js
@@ -16,12 +16,52 @@ document.addEventListener('DOMContentLoaded', function () {
         xhr.setRequestHeader('accept', 'application/json');
         xhr.setRequestHeader('content-type', 'application/json');
         xhr.addEventListener('readystatechange', function () {
-            if( xhr.readyState === 4 && xhr.status === 200 ) {
+            if( xhr.readyState === 4 && xhr.status === 201 ) {
+                console.log('malformed json');
                 var obj = JSON.parse(xhr.responseText);
                 console.log(obj);
+
+                var contacts_container = document.querySelector('#allcontacts');
+                addContactToView(contacts_container, jsonData);
             }
         });
         xhr.send(JSON.stringify(jsonData));
     });
 
+    // display all the contacts on the page
+    updateAllContactsView();
+
 });
+
+function updateAllContactsView() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', '/contacts');
+    xhr.setRequestHeader('accept', 'application/json');
+    xhr.addEventListener('readystatechange', function () {
+        if( xhr.readyState === 4 && xhr.status === 200 ) {
+            var contacts = JSON.parse(xhr.responseText);
+            console.log(contacts);
+
+            var contacts_container = document.querySelector('#allcontacts');
+            contacts.forEach(function (contact) {
+                addContactToView(contacts_container, contact);
+            });
+
+        }
+    });
+    xhr.send();
+}
+
+function addContactToView(contacts_container, contact) {
+    var contact_div = createElement('div', contacts_container, '', 'contact-container');
+    createElement('span', contact_div, contact.name, 'contact-name');
+    createElement('span', contact_div, contact.location, 'contact-name');
+}
+
+function createElement(type, parent, innerHTML, className) {
+    var element = document.createElement(type);
+    if (innerHTML) element.innerHTML = innerHTML;
+    if (className) element.className = className;
+    if (parent) parent.appendChild(element);
+    return element;
+}
